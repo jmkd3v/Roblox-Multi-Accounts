5d874904fcdc5a0803dbde670a4382360938fb2c
jmkd3v <jmk@jmksite.dev>
2021-07-23 23:13:50 -0400

    added injection

---
+++ scripts/inject.js
@@ -0,0 +1,16 @@
+/*
+
+RobloxMultiAccounts
+by jmkdev (https://jmk.gg/)
+
+inject.js
+This inserts the script.js file into the roblox.com website.
+
+*/
+
+let headElement = (document.head || document.documentElement);
+
+let injectedScript = document.createElement('script');
+injectedScript.src = chrome.runtime.getURL('scripts/script.js');
+injectedScript.classList.add("multi-accounts-injected-by-jmk-💖");
+headElement.appendChild(injectedScript);
---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+Roblox.Dialog.open({
+    titleText: "Roblox Multi Accounts",
+    bodyContent: `Please choose an account to swap to.
+<div class="account-store">
+    <button class="account">
+        <img class="account-image" src="https://tr.rbxcdn.com/2b4ab4eee0e5531f990ccf6aac76c440/150/150/AvatarHeadshot/Png">
+        <p>local_ip</p>
+    </button>
+</div>`,
+    acceptText: "Add",
+    declineText: "Settings",
+    allowHtmlContentInBody: true
+})
