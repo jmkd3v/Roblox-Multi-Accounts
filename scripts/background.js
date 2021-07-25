781b059e96a962d1c1e7f5e55dbf04dd68dc981f
jmkd3v <jmk@jmksite.dev>
2021-07-24 23:47:50 -0400

    added lots of boilerplate from old stuff to background script

--- scripts/background.js
+++ scripts/background.js
@@ -9,6 +9,47 @@ For the main code, see popup.js.
 
 */
 
+import * as userTokenElements from "../modules/user-token-elements.js"
+import * as userTokens from "../modules/user-tokens.js"
+import * as token from "../modules/token.js"
+import * as store from "../modules/store.js"
+import * as rbx from "../modules/rbx.js"
+import { updateTokens } from "../modules/upgrade.js"
+import { versionCompare } from "../modules/version-compare.js"
+
+const version = chrome.runtime.getManifest().version;		// get current extension version from manifest
+const storeKey = "rbxtokens"; 								// key to look for tokens in
+	
+let storeVersion = (await store.get("version")).version;	// get the version of the extension that last saved data into storage
+let userTokenStore = (await store.get(storeKey))[storeKey];	// get the tokens from chrome sync storage
+let versionDiff = -1;										// versionDiff defaults to -1
+let isOld = true;											// set to true if the data is pre-1.0 and needs to be updated
+
+if (!userTokenStore) {  // If there isn't any userTokenStore...
+	// ...we know that this is a fresh install and don't have to upgrade anything
+	isOld = false;
+
+	userTokenStore = {}
+} else if (storeVersion) {  // If a version exists in the DB...
+	// ...we know that the data stored was from post-rewrite, so we can set isOld to false
+	isOld = false;
+
+	 // Compare the DB stored version and the current version
+	versionDiff = versionCompare(storeVersion, version); 
+}
+
+if (isOld) {
+	// This means the data comes from before the new format was introduced.
+	// We'll need to update it before using it.
+	
+	let oldTokenStore = userTokenStore[0];  // the old format stored an Array under the storeKey containing the tokens
+
+	const t0 = performance.now();
+	userTokenStore = await updateTokens(oldTokenStore);  // update pre-rewrite tokens and convert them to v1 tokens
+	const t1 = performance.now();
+	console.log(`Call took ${t1 - t0} milliseconds.`);
+}
+
 chrome.runtime.onInstalled.addListener(function () {
 	chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
 		chrome.declarativeContent.onPageChanged.addRules([
