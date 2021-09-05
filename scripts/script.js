a8307cf79b5903b261f762ef83db5a2a08e8775d
jmkd3v <jmk@jmksite.dev>
2021-09-05 16:52:29 -0300

    ag

--- scripts/script.js
+++ scripts/script.js
@@ -1,30 +1,30 @@
 function generateAccount(account) {
-	// TODO: React.
-	let baseElement = document.createElement("button");
-	baseElement.classList.add("account");
+  // TODO: React.
+  let baseElement = document.createElement("button");
+  baseElement.classList.add("account");
 
-	let imageElement = document.createElement("img");
-	imageElement.classList.add("account-image");
-	imageElement.src = account.image;
+  let imageElement = document.createElement("img");
+  imageElement.classList.add("account-image");
+  imageElement.src = account.image;
 
-	let nameContainer = document.createElement("div");
-	nameContainer.classList.add("account-name-container")
+  let nameContainer = document.createElement("div");
+  nameContainer.classList.add("account-name-container");
 
-	let displayNameElement = document.createElement("p");
-	displayNameElement.innerHTML = account.displayName;
-	displayNameElement.classList.add("account-display-name");
+  let displayNameElement = document.createElement("p");
+  displayNameElement.innerHTML = account.displayName;
+  displayNameElement.classList.add("account-display-name");
 
-	let nameElement = document.createElement("p");
-	nameElement.innerHTML = account.name;
-	nameElement.classList.add("account-name");
+  let nameElement = document.createElement("p");
+  nameElement.innerHTML = account.name;
+  nameElement.classList.add("account-name");
 
-	nameContainer.appendChild(displayNameElement);
-	nameContainer.appendChild(nameElement);
+  nameContainer.appendChild(displayNameElement);
+  nameContainer.appendChild(nameElement);
 
-	baseElement.appendChild(imageElement);
-	baseElement.appendChild(nameContainer);
+  baseElement.appendChild(imageElement);
+  baseElement.appendChild(nameContainer);
 
-	return baseElement;
+  return baseElement;
 }
 
 const blurObscureOverlay = document.createElement("div");
@@ -36,91 +36,112 @@ hideObscureOverlay.classList.add("hide-obscure-overlay");
 document.body.appendChild(hideObscureOverlay);
 
 function enableBlurObscure() {
-	let html = document.documentElement;
-	/*
+  let html = document.documentElement;
+  /*
 	let yOffset = html.scrollTop + (html.clientHeight/2);
 	html.style.transformOrigin = "50vw " + yOffset + "px";
 	html.classList.add("obscure");
 	*/
-	blurObscureOverlay.classList.add("obscure");
-	// html.style.overflow = "hidden";
+  blurObscureOverlay.classList.add("obscure");
+  // html.style.overflow = "hidden";
 }
 
 function disableBlurObscure() {
-	let html = document.documentElement;
-	/*
+  let html = document.documentElement;
+  /*
 	html.style.overflow = "hidden";
 	html.classList.remove("obscure");
 	*/
-	blurObscureOverlay.classList.remove("obscure");
-	setTimeout(() => {
-		// html.style.transformOrigin = "";
-		// html.style.overflow = "";
-	}, 260)
+  blurObscureOverlay.classList.remove("obscure");
+  setTimeout(() => {
+    // html.style.transformOrigin = "";
+    // html.style.overflow = "";
+  }, 260);
+}
+
+function closeAccountDialog(full = true) {
+  disableBlurObscure();
+  if (full) {
+    Roblox.Dialog.close();
+  }
 }
 
 function openAccountDialog() {
-	Roblox.Dialog.close();
-	Roblox.Dialog.open({
-		titleText: "Roblox Multi Accounts",
-		bodyContent: `Please choose an account to swap to.
+  Roblox.Dialog.close();
+  Roblox.Dialog.open({
+    titleText: "Roblox Multi Accounts",
+    bodyContent: `Please choose an account to swap to.
     <div class="account-store">
 
     </div>`,
-		acceptText: "Add",
-		declineText: "Settings",
-		allowHtmlContentInBody: true,
-	});
-    
-    // this is a hack because roblox dialogs suck lol
-    let modalContainer = document.getElementById("simplemodal-container");
-    modalContainer.classList.add("custom-modal");
-	
-	enableBlurObscure();
+    xToCancel: true,
+    acceptText: "Add",
+    declineText: "Settings",
+    allowHtmlContentInBody: true,
+    onDecline: () => {
+      alert("decline");
+    },
+    onAccept: () => {
+      alert("accept");
+    },
+    onCancel: () => {},
+    onCloseCallback: () => {
+      closeAccountDialog(false);
+    },
+  });
 
-	let accountStore = document.getElementsByClassName("account-store")[0];
-	let getAccountsEvent = new CustomEvent("PassToBackground", {
-		detail: {
-			question: "getAccounts",
-		},
-	});
+  // this is a hack because roblox dialogs suck lol
+  let modalContainer = document.getElementById("simplemodal-container");
+  modalContainer.classList.add("custom-modal");
 
-	window.addEventListener(
-		"FG_giveAccounts",
-		function (evt) {
-			let accounts = evt.detail.accounts;
-			console.log(accounts);
-			for (let account of accounts) {
-                let accountElement = generateAccount(account);
-                accountElement.addEventListener("click", function() {
-					disableBlurObscure();
-                    Roblox.Dialog.close();
+  /*
+  let modalOverlay = document.getElementById("simplemodal-overlay");
+  modalOverlay.click = function () {}; // Disable overlay handler
+  */
 
-                    let accountChosenEvent = new CustomEvent("PassToBackground", {
-                        detail: {
-                            question: "accountChosen",
-                            id: account.id
-                        },
-                    });
-                
-                });
+  enableBlurObscure();
 
-				accountStore.appendChild(accountElement);
-			}
-		},
-		{ once: true }
-	);
+  let accountStore = document.getElementsByClassName("account-store")[0];
+  let getAccountsEvent = new CustomEvent("PassToBackground", {
+    detail: {
+      question: "getAccounts",
+    },
+  });
 
-	window.dispatchEvent(getAccountsEvent);
+  window.addEventListener(
+    "FG_giveAccounts",
+    function (evt) {
+      let accounts = evt.detail.accounts;
+      console.log(accounts);
+      for (let account of accounts) {
+        let accountElement = generateAccount(account);
+        accountElement.addEventListener("click", function () {
+          Roblox.Dialog.close();
+
+          let accountChosenEvent = new CustomEvent("PassToBackground", {
+            detail: {
+              question: "accountChosen",
+              id: account.id,
+            },
+          });
+        });
+
+        accountStore.appendChild(accountElement);
+      }
+    },
+    { once: true }
+  );
+
+  window.dispatchEvent(getAccountsEvent);
 }
 
 window.addEventListener("FG_openAccountDialog", function (evt) {
-	openAccountDialog();
+  openAccountDialog();
 });
 
 let readyEvent = new CustomEvent("PassToBackground", {
-	detail: {
-		question: "ready",
-	},
+  detail: {
+    question: "ready",
+  },
 });
 window.dispatchEvent(readyEvent);
